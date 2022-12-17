c28d20552e39ac936ba411264b35d0fe03f1a627
eq19 <admin@eq19.com>
2022-12-17 15:58:02 +0700

    Update github_pages.sh

--- Journal/Scripts/github_pages.sh
+++ Journal/Scripts/github_pages.sh
@@ -2,7 +2,7 @@
 
 if [[ "$sm_path" != */* ]]; then 
   REPOSITORY=$(git config remote.origin.url)
-  REPOSITORY=${REPOSITORY/"https://github.com/"/""}
+  export REPOSITORY=${REPOSITORY/"https://github.com/"/""}
   echo -e "Deploying to ${REPOSITORY} on branch ${BRANCH}"
   cd ${WORKING_DIR}/build && rm -rf .git
   git init && touch .nojekyll && deploy_remote
