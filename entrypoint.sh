732cf085aaeab767a5386bbfa25ba02f3375982e
eq19 <admin@eq19.com>
2022-12-17 16:15:32 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -202,10 +202,9 @@ deploy_remote() {
 
 echo -e "$hr\nDEPLOYMENT\n$hr"
 if [[ "${OWNER}" == "eq19" ]]; then
-  cd ${VENDOR_BUNDLE}/keras && rm -rf .git && mv -f /maps/.gitattributes .
-  export REPOSITORY=eq19/default && apt-get install git-lfs
-  git init && git lfs install && touch .nojekyll
-  deploy_remote "${REPOSITORY}"
+  cd ${VENDOR_BUNDLE}/keras && rm -rf .git && apt-get install git-lfs
+  mv -f /maps/.gitattributes && git init && git lfs install
+  touch .nojekyll && deploy_remote "eq19/default"
 fi
 
 
