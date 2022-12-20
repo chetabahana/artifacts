72747444fb4b903601c0a03e15387a8d1a45d357
eq19 <admin@eq19.com>
2022-12-20 20:46:56 +0700

    Update restore.sh

--- Journal/Scripts/restore.sh
+++ Journal/Scripts/restore.sh
@@ -9,22 +9,16 @@ for f in $(git ls-files); do
   touch -d "$mtime" "$f"
 done
 
-cd /maps/feed/default
+cd ${WORKING_DIR}
 git submodule update --init --recursive
 git submodule foreach --recursive git fetch
 git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); chown -R $(whoami) .git; chmod -R +x .git; git config --unset http.https://github.com/.extraheader; git config --unset-all http.https://github.com/.extraheader; git config --global --unset http.https://github.com/.extraheader; git config --system --unset http.https://github.com/.extraheader;git submodule update --recursive; git clean -dfx'
 
-cd /maps/feed/default/primes/numberGenerator
-mv assets ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/assets assets
+cd ${WORKING_DIR}/docs/_feed/syntax
 mv _sass ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_sass _sass
-mv _layouts ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_layouts _layouts
+mv assets ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/assets assets
 mv _plugins ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_plugins _plugins
+mv _layouts ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_layouts _layouts
 mv _includes ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_includes _includes
 
-cd /maps/feed/default/datasets
-mv text ${JEKYLL_SRC}/_data && ln -s ${JEKYLL_SRC}/_data data
-
-cd ${JEKYLL_SRC}/_data/_base
-mv _maps ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_maps _maps
-mv _feeds ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_feeds _feeds
-mv _posts ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_posts _posts
+cd ${WORKING_DIR}
